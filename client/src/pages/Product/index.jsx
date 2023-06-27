dfbb6f5214015aebdc941c664620d48b03a2d0f3
techshomez <danieltopeg@gmail.com>
2023-06-27 14:14:24 +0100

    product details page

--- client/src/pages/Product/index.jsx
+++ client/src/pages/Product/index.jsx
@@ -1,7 +1,12 @@
-import "./product.css";
+import "./product.scss";
 
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <div className="product">
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default ProductPage;
