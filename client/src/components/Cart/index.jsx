c9e81cf07bf640f1ff3e50b286ee00e9c9bc55b4
techshomez <danieltopeg@gmail.com>
2023-06-28 15:00:58 +0100

    cart dropdown component

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,7 +1,45 @@
 import "./cart.scss";
+import { FaTimes } from "react-icons/fa";
+
+const data = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-shirt",
+    desc: "Long Sleeve Graphic T-shirt",
+    isNew: true,
+    oldPrice: 19,
+    price: 12,
+  },
+];
 
 const Cart = () => {
-  return <div className="cart">Cart</div>;
+  // ScrollReveal().reveal(".cart");
+  return (
+    <div className="cart">
+      <h1>Cart Lists</h1>
+      {data?.map((item) => {
+        return (
+          <div className="item" key={item.id}>
+            <img src={item.img} alt="" />
+            <div className="details">
+              <h1>{item.title}</h1>
+              <p>{item.desc.substring(0, 100)}</p>
+              <div className="price">1 x ${item.price}</div>
+            </div>
+            <FaTimes className="delete" />
+          </div>
+        );
+      })}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$123</span>
+      </div>
+      <button>PROCEED TO CHECKOUT</button>
+      <span className="reset">Empty Cart</span>
+    </div>
+  );
 };
 
 export default Cart;
